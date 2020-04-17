6e5ff4ed2b9d8151b028a33422ad52a747019f1d
Arjun <adhaliwal111@my.bcit.ca>
2020-04-17 10:20:12 -0700

    Made getters

--- user.py
+++ user.py
@@ -6,10 +6,25 @@ import doctest
 
 class User:
 
-    def __init__(self, name: str, age: int, income: list, country: str, student: bool):
+    def __init__(self, name: str, age: int, income: int, country: str, student: bool):
         self.__name = name
         self.__age = age
         self.__income = income
         self.__country = country
         self.__student = student
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_age(self) -> int:
+        return self.__age
+
+    def get_income(self) -> int:
+        return self.__income
+
+    def get_country(self) -> str:
+        return self.__country
+
+    def get_student(self) -> bool:
+        return self.__student
 
