80855e49702064dd8f3ceb47fb21e400a7b958c6
jeessicahong <32560377+jeessicahong@users.noreply.github.com>
2020-04-17 10:33:00 -0700

    Added try except block and parsed JSON string to Python object (dict) in verify_news_api_response

--- news.py
+++ news.py
@@ -1,18 +1,22 @@
 # e454fda9cf5b4a5d8e6f8bc3d960c7b5
+import json
+
 import requests
 
 
-def verify_news_api_response(url):
+def verify_news_api_response(url: str) -> dict:
     response = requests.get(url)
     response.raise_for_status()
 
-    if response.status_code == 200:
-        print("Success!")
+    try:
+        response.raise_for_status()
     # Bad Request. The request was unacceptable, often due to a missing or misconfigured parameter
-    elif response.status_code == 400:
-        print("Not found")
+    except requests.exceptions.HTTPError as e:
+        print(e)
+    else:
+        return json.loads(response.text)
 
 
-def get_country_news(country_code):
-    verify_news_api_response("https://newsapi.org/v2/top-headlines?country="
-                             + country_code + "&apiKey=e454fda9cf5b4a5d8e6f8bc3d960c7b5")
+def get_country_news(country_code: str):
+    country_news_info = verify_news_api_response("https://newsapi.org/v2/top-headlines?country="
+                                                 + country_code + "&apiKey=e454fda9cf5b4a5d8e6f8bc3d960c7b5")
