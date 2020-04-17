e4cb3e0795ee2eea6f011b41f5a5e9d0488d6b4b
jeessicahong <32560377+jeessicahong@users.noreply.github.com>
2020-04-17 16:05:26 -0700

    Added verification for user's age, country, income, province and student status functions

--- funding.py
+++ funding.py
@@ -9,3 +9,34 @@ def verify_for_funding(user):
 
     if all(value for value in verification.values()):
         print("You are verified!")
+
+
+def verify_country(user):
+    return True if user.get_country() == "Canada" else False
+
+
+def verify_age(user):
+    return True if user.get_age() >= 15 else False
+
+
+def verify_income(user):
+    if user.get_income() >= 5000:
+        return verify_province()
+    else:
+        if verify_if_student(user):
+            open_link("")
+
+
+def verify_province():
+    user_province = input("What province do you currently reside in? (EX. BC) ").upper().strip()
+
+    print(user_province)
+    return True if user_province == "BRITISH COLUMBIA" or user_province == "BC" else False
+
+
+def verify_if_student(user):
+    return True if user.get_student() else False
+
+
+def open_link(url):
+    pass
