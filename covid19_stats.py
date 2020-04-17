e49d9c8b9da07c31eda0003be2c5a69063cfd27e
Hudson McManus <hudson.mcmanus@gmail.com>
2020-04-17 11:36:48 -0700

    Moved all covid19 api requests to covid19_stats.py

--- covid19_stats.py
+++ covid19_stats.py
@@ -1,6 +1,24 @@
 import doctest
 import requests
+import json
 
+
+def get(api_link):
+    response = requests.get(api_link)
+    response.raise_for_status()
+    return json.loads(response.text)
+
+
+def global_stats():
+    return get('https://api.covid19api.com/summary')
+
+
+def get_countries():
+    return get('https://api.covid19api.com/countries')
+
+
+def get_country_stats(country):
+    return next(item for item in get('https://api.covid19api.com/summary')['Countries'] if item["Slug"] == country)
 
 
 def main():
