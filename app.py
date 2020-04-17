666340e7c2ae8c69758e01d8f32efe924bbc314b
Hudson McManus <hudson.mcmanus@gmail.com>
2020-04-17 10:26:25 -0700

    Added get function to gen a response from the API

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ import json
 import textwrap
 
 
+def get(api_link):
+    response = requests.get(api_link)
+    response.raise_for_status()
+    return json.loads(response.text)
 
 
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
