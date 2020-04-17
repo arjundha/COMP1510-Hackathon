735f890c1ca3729e70e49596d19eb1f93d4e7da2
Hudson McManus <hudson.mcmanus@gmail.com>
2020-04-17 14:50:15 -0700

    Added comments covid19_stats.py

--- covid19_stats.py
+++ covid19_stats.py
@@ -4,12 +4,16 @@ import json
 
 
 def get(api_link):
+    # Get response from the api
     response = requests.get(api_link)
+
+    # Check status of response
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print(e)
     else:
+        # Return json dictionary
         return json.loads(response.text)
 
 
@@ -18,6 +22,7 @@ def global_stats():
 
 
 def get_country_stats(country):
+    # Search list for specified country key
     return next(item for item in get('https://api.covid19api.com/summary')['Countries'] if
                 item["Country"].lower() == country.strip().lower())
 
