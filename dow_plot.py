212141e5a689bc73e877a19b849fdfb7c5c08622
Dkeen <duncankeen@gmail.com>
2020-04-17 18:00:46 -0700

    docstrings and ignoring futurewarning for dow_plot

--- dow_plot.py
+++ dow_plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plot
+import warnings
+warnings.simplefilter(action="ignore", category=FutureWarning)
 from matplotlib import style
 import datetime
 import pandas as pd
@@ -6,22 +8,43 @@ import pandas_datareader.data as web
 
 
 def create_csv(start, end):
+    """Create a csv files with the financial data of the DOW jones from 2020-1-1 to 2020-4-17.
+
+    :param start: an object containing the datetime on January 1st, 2020
+    :param end: an object containing the datetime on January 1st, 2020
+    :precondition: start and end must be well-formed datetime objects
+    :postcondition: a csv file has successfully been created and the financial history of the DOW jones over the
+                    specified period has been written to it
+    """
     data_frame = web.DataReader("^DJI", "yahoo", start, end)
     data_frame.to_csv("dow.csv")
 
 
 def read_csv():
-    # reading data from csv file, parsing the Dates to make the x-axis, setting index_col to zero to remove it
+    """Read from the dow.csv file.
+    
+    :precondition: a well formed csv file named dow.csv exists
+    :postcondition: successfully returns the data in the dow.csv file
+    :return: the data in the dow.csv file
+    """
     csv_file = "dow.csv"
+
+    # read the data from the csv file, parsing the Dates to make the x-axis, setting index_col to zero to remove it
     data_frame = pd.read_csv(csv_file, parse_dates=True, index_col=0)
     return data_frame
 
 
 def plot_data(data_frame):
-    # plotting the price at Close of the dow after each day
+    """Plot the daily closes for the DOW Jones over the past 4 months.
+
+    :param data_frame: the data contained in the dow.cvs file
+    :precondition: data_frame must be well-formed
+    :postcondition: the daily closes of the dow over the past 4 months have been correctly plotted
+    """
+    # plot the price at daily close of the dow after each day
     data_frame["Close"].plot()
 
-    # showing the plot
+    # show the plot
     plot.show()
 
 
@@ -29,6 +52,7 @@ def main():
     style.use("ggplot")
     start = datetime.datetime(2020, 1, 1)
     end = datetime.datetime(2020, 4, 17)
+
     create_csv(start, end)
     data_frame = read_csv()
     plot_data(data_frame)
