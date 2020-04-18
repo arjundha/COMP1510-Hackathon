b25344a21d50f0d77944249fa041c4f7bc059d02
Hudson McManus <hudson.mcmanus@gmail.com>
2020-04-17 19:53:41 -0700

    Added doctest to get_country

--- covid19_stats.py
+++ covid19_stats.py
@@ -1,6 +1,7 @@
 import doctest
 import requests
 import json
+import doctest
 
 
 def get(api_link):
@@ -43,6 +44,11 @@ def get_country_stats(country):
     :precondition: country must be a string
     :postcondition: will search through the dictionary of countries from api for the country
     :return: the country as a string
+
+    >>> get_country_stats('canada')
+    {'Country': 'Canada', 'CountryCode': 'CA', 'Slug': 'canada', 'NewConfirmed': 2005, 'TotalConfirmed': 32814, 'NewDeaths': 97, 'TotalDeaths': 1355, 'NewRecovered': 847, 'TotalRecovered': 10545, 'Date': '2020-04-18T02:46:42Z'}
+    >>> get_country_stats('United States of America')
+    {'Country': 'United States of America', 'CountryCode': 'US', 'Slug': 'united-states', 'NewConfirmed': 31855, 'TotalConfirmed': 699148, 'NewDeaths': 3853, 'TotalDeaths': 36758, 'NewRecovered': 3842, 'TotalRecovered': 58545, 'Date': '2020-04-18T02:46:42Z'}
     """
     # Search list for specified country key
     return next(item for item in get('https://api.covid19api.com/summary')['Countries'] if
